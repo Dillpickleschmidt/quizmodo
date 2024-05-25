aa67aaf0cfb09fd756e097da54b1559b68fe4c3a
Lijie Chen <331533198dx@gmail.com>
2024-05-24 22:17:05 -0400

    crate a fake data

--- src/app/(service)/problemService.js
+++ src/app/(service)/problemService.js
@@ -1,29 +1,39 @@
-import { db } from "./db";
-
-const collectionName = 'problems';
+const fackQuestions = {
+    "What is the capital of France?": {
+      "answer": ["Paris"],
+      "mnemonics": ["Paris rhymes with 'pair of keys', which can unlock the gates to the city of lights."],
+      "notes": ["Paris is also known for its significant contributions to art, fashion, and cuisine."]
+    },
+    "What is the boiling point of water?": {
+      "answer": ["100 degrees Celsius"],
+      "mnemonics": ["'Boiling point' sounds like 'boiling joint', imagine water boiling at a joint family dinner at 100 degrees."],
+      "notes": ["This is at standard atmospheric pressure."]
+    },
+    "What is the chemical symbol for gold?": {
+      "answer": ["Au"],
+      "mnemonics": ["'Au' sounds like 'ow', something you might exclaim when finding gold because it's so valuable."],
+      "notes": ["Gold has been a valuable resource for jewelry and economics for centuries."]
+    },
+    "Who wrote 'To Kill a Mockingbird'?": {
+      "answer": ["Harper Lee"],
+      "mnemonics": ["'Harper' sounds like 'harp', and a musician could sing about the mockingbird."],
+      "notes": ["This novel was published in 1960 and is famous for its themes of racial injustice."]
+    }
+  }
+  
 
 export async function getProblems() {
-  const querySnapshot = await getDocs(collection(collectionName, db));
-  return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    return fackQuestions;
 }
 
 export async function addProblem(problem) {
-  const docRef = await addDoc(collection(db, collectionName), problem);
-  return { id: docRef.id, ...problem };
 }
 
 export async function updateProblem(id, updatedData) {
-  const docRef = doc(db, collectionName, id);
-  await updateDoc(docRef, updatedData);
-  return { id, ...updatedData };
 }
 
 export async function deleteProblem(id) {
-  await deleteDoc(doc(db, collectionName, id));
 }
 
 export async function getProblembyid(id) {
-    const docRef = doc(db, collectionName, id);
-    const docSnap = await getDoc(docRef);
-    return docSnap.data();
 }
