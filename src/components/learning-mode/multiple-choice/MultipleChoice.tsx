664425399fbb835b9a2c539f33580696f58cd39f
Dylan Kuzmick <drkuzmick@gmail.com>
2024-05-25 19:11:29 -0400

    Added user feedback from answer selection & future proofed multiple-choice functions

--- src/components/learning-mode/multiple-choice/MultipleChoice.tsx
+++ src/components/learning-mode/multiple-choice/MultipleChoice.tsx
@@ -1,32 +1,29 @@
+import React, { useEffect, useMemo } from "react"
 import { View } from "react-native"
-import React, { useEffect } from "react"
 import data from "@/test-data/test-data.json"
-import {
-  handleMultipleChoiceSelection,
-  presentMultipleChoiceOptions,
-  MultipleChoices,
-} from "./multiple-choice"
+import { handleMultipleChoiceSelection, presentMultipleChoiceOptions } from "./multiple-choice"
 import { Button } from "@/components/ui/button"
 import { Text } from "@/components/ui/text"
 import { useLearningModeContext } from "@/context/LearningModeContext"
 
 export default function MultipleChoice() {
-  const { setCorrectKey } = useLearningModeContext()
-  const choices = presentMultipleChoiceOptions(data)
-  setCorrectKey(choices.correctKey)
+  const { setCorrectKey, setIsSelectionCorrect, setHasUserAnswered } = useLearningModeContext()
+
+  const choices = useMemo(() => presentMultipleChoiceOptions(data), [data])
+
+  useEffect(() => {
+    setCorrectKey(choices.correctKey)
+  }, [choices.correctKey, setCorrectKey])
 
   const handleButtonPress = (selection: string) => {
-    console.log("User selected: " + selection)
-    const isCorrect = handleMultipleChoiceSelection(choices, selection)
-    console.log("Selection is correct: " + isCorrect)
+    setIsSelectionCorrect(handleMultipleChoiceSelection(choices, selection))
+    setHasUserAnswered(true)
   }
 
   return (
-    <View className="mt-4 -mb-24">
+    <View className="mt-4">
       <Text>Multiple Choice</Text>
       {choices.options.map((option, index) => (
-        // Display the answers as buttons
-        // Choose the first answer [0] from the array arbitrarily since they're all correct anyway
         <Button key={index} onPress={() => handleButtonPress(option.answers[0])} className="my-2">
           <Text>{option.answers.join(", ")}</Text>
         </Button>
